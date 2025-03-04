cbb2dcb1004545903a933562886af83f7acaabb2
[Dillon Reading] <dillreading@gmail.com>
2025-03-04 11:52:56 -0700

    working on endpoints

--- destinations/core/views.py
+++ destinations/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from models import Destination
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    destinations = Destination.objects.filter(share_publicly=True).order_by('-id')[:5]
+    return render(request, "core/index.html", {"destinations": destinations})
+
+

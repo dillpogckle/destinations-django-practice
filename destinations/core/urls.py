72df313924eabe8c1e1c5ba1eb484a5a75bb5991
[Dillon Reading] <dillreading@gmail.com>
2025-03-04 13:45:00 -0700

    user creation page

--- destinations/core/urls.py
+++ destinations/core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new_user/", views.new_user, name="new_user"),
+    path("users/", views.create_user, name="create_user"),
+
 ]
--- destinations/core/views.py
+++ destinations/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from models import Destination
+from .models import Destination, User
 from django.http import HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
+from django.contrib.auth.hashers import make_password, check_password
 
 
 def index(request: HttpRequest):
@@ -8,3 +9,21 @@ def index(request: HttpRequest):
     return render(request, "core/index.html", {"destinations": destinations})
 
 
+def new_user(request: HttpRequest):
+    return render(request, "core/new_user.html")
+
+
+def create_user(request :HttpRequest):
+    # Validation
+    name = request.POST["name"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+    if name == "" or email == "" or password == "":
+        return HttpResponseBadRequest("All fields must be filled <br> <a href='/new_user/'>Go back</a>")
+    if User.objects.filter(email=email).exists():
+        return HttpResponseBadRequest("Email already exists <br> <a href='/new_user/'>Go back</a>")
+
+    # Create user
+    user = User(name=name, email=email, password_hash=make_password(password))
+    user.save()
+    return HttpResponseRedirect("/")
